ff225f42316d0076f65d54d14f600fd7a1d848c0
izumin5210 <masayuki@izumin.info>
2016-07-13 20:42:22 +0900

    "Initial commit"

---
+++ src/AICPHelperApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class AICPHelperApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void AICPHelperApp::setup()
+{
+}
+
+void AICPHelperApp::mouseDown( MouseEvent event )
+{
+}
+
+void AICPHelperApp::update()
+{
+}
+
+void AICPHelperApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP( AICPHelperApp, RendererGl )
